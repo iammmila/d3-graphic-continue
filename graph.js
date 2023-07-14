774ed1d8557d89a5dcb5ff290c39440e27f89f03
iammmila <ibramila2002@gmail.com>
2023-07-14 11:28:19 +0400

    second time - x and y scales are solved

--- graph.js
+++ graph.js
@@ -29,113 +29,6 @@ d3.csv("./data.csv")
       d.Year = String(d.Year);
       d.Rank = +d.Rank;
     });
-
-    // Define the color scale
-    const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
-    // Extract the unique country names
-    const countries = [...new Set(data.map((d) => d.Country))];
-    // Iterate over each country
-    x.domain(
-      d3.extent(data, function (d) {
-        return new Date(d.Year);
-      })
-    );
-    y.domain([
-      0,
-      d3.max(data, function (d) {
-        return d.Rank;
-      }),
-    ]);
-
-    countries.forEach(function (country, i) {
-      // Filter the data for the current country
-      const filteredData = data.filter(function (d) {
-        return d.Country === country;
-      });
-
-      function updateGraph() {
-        const selectedSector = selectSector.property("value");
-        const selectedSubsector = selectSubsector.property("value");
-        const selectedIndicator = selectIndicator.property("value");
-        let filteredData = data;
-
-        if (selectedSector) {
-          filteredData = filteredData.filter(
-            (d) => d.Sector === selectedSector
-          );
-        }
-        if (selectedSubsector) {
-          filteredData = filteredData.filter(
-            (d) => d.Subsector === selectedSubsector
-          );
-        }
-        if (selectedIndicator) {
-          filteredData = filteredData.filter(
-            (d) => d.Indicator === selectedIndicator
-          );
-        } else {
-          filteredData = filteredData;
-        }
-
-        // Clear the previous graph
-        svg.selectAll("path").remove();
-
-        // Update the graph with the filtered data
-        const filteredCountries = [
-          ...new Set(filteredData.map((d) => d.Country)),
-        ];
-
-        filteredCountries.forEach(function (country, i) {
-          const countryData = filteredData.filter((d) => d.Country === country);
-          const line = d3
-            .line()
-            .x(function (d) {
-              return x(new Date(d.Year));
-            })
-            .y(function (d) {
-              return y(d.Rank);
-            });
-          // Update the x-axis
-          svg.select(".x-axis").call(d3.axisBottom(x));
-
-          // Update the y-axis
-          svg.select(".y-axis").call(d3.axisLeft(y));
-
-          svg
-            .append("path")
-            .datum(countryData)
-            .attr("fill", "none")
-            .attr("stroke", colorScale(i))
-            .attr("stroke-width", 3)
-            .attr("d", line);
-        });
-      }
-
-      updateGraph();
-      // Add the x-axis
-      svg
-        .append("g")
-        .attr("class", "x-axis")
-        .attr("transform", `translate(0, ${height})`)
-        .call(d3.axisBottom(x));
-
-      // Add the y-axis
-      svg.append("g").attr("class", "y-axis").call(d3.axisLeft(y));
-      
-      selectSector.on("change", updateGraph);
-      selectSubsector.on("change", updateGraph);
-      selectIndicator.on("change", updateGraph);
-    });
-
-    // Add the x-axis
-    svg
-      .append("g")
-      .attr("transform", `translate(0, ${height})`)
-      .call(d3.axisBottom(x));
-
-    // Add the y-axis
-    svg.append("g").call(d3.axisLeft(y));
-
     const Countries = [...new Set(data.map((d) => d.Country))];
     const Sectors = [...new Set(data.map((d) => d.Sector))];
     const Subsector = [...new Set(data.map((d) => d.Subsector))];
@@ -172,6 +65,111 @@ d3.csv("./data.csv")
       .append("option")
       .text((d) => d)
       .attr("value", (d) => d);
+    // Define the color scale
+    const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+    // Extract the unique country names
+    const countries = [...new Set(data.map((d) => d.Country))];
+    // Iterate over each country
+    x.domain(
+      d3.extent(data, function (d) {
+        return new Date(d.Year);
+      })
+    );
+    y.domain([
+      0,
+      d3.max(data, function (d) {
+        return d.Rank;
+      }),
+    ]);
+
+    countries.forEach(function (country, i) {
+      // Filter the data for the current country
+      const filteredData = data.filter(function (d) {
+        return d.Country === country;
+      });
+
+      function updateGraph() {
+        const selectedSector = selectSector.property("value");
+        const selectedSubsector = selectSubsector.property("value");
+        const selectedIndicator = selectIndicator.property("value");
+        let filteredData = data;
+        const filteredCountries = [
+          ...new Set(filteredData.map((d) => d.Country)),
+        ];
+
+        if (selectedSector) {
+          filteredData = filteredData.filter(
+            (d) => d.Sector === selectedSector
+          );
+        }
+        if (selectedSubsector) {
+          filteredData = filteredData.filter(
+            (d) => d.Subsector === selectedSubsector
+          );
+        }
+        if (selectedIndicator) {
+          filteredData = filteredData.filter(
+            (d) => d.Indicator === selectedIndicator
+          );
+        } else {
+          filteredData = filteredData;
+        }
+
+        // Clear the previous graph
+        svg.selectAll("path").remove();
+
+        // Update the graph with the filtered data
+        filteredCountries.forEach(function (country, i) {
+          const countryData = filteredData.filter((d) => d.Country === country);
+          const line = d3
+            .line()
+            .x(function (d) {
+              return x(new Date(d.Year));
+            })
+            .y(function (d) {
+              return y(d.Rank);
+            });
+
+          // Update the x-axis
+          svg.select(".x-axis").call(d3.axisBottom(x));
+
+          // Update the y-axis
+          svg.select(".y-axis").call(d3.axisLeft(y));
+
+          svg
+            .append("path")
+            .datum(countryData)
+            .attr("fill", "none")
+            .attr("stroke", colorScale(i))
+            .attr("stroke-width", 3)
+            .attr("d", line);
+        });
+      }
+
+      updateGraph();
+      // Add the x-axis
+      svg
+        .append("g")
+        .attr("class", "x-axis")
+        .attr("transform", `translate(0, ${height})`)
+        .call(d3.axisBottom(x));
+
+      // Add the y-axis
+      svg.append("g").attr("class", "y-axis").call(d3.axisLeft(y));
+
+      selectSector.on("change", updateGraph);
+      selectSubsector.on("change", updateGraph);
+      selectIndicator.on("change", updateGraph);
+    });
+
+    // Add the x-axis
+    svg
+      .append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(d3.axisBottom(x));
+
+    // Add the y-axis
+    svg.append("g").call(d3.axisLeft(y));
   })
   .catch(function (error) {
     console.log("Error:", error);
